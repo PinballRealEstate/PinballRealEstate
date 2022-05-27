8298768fd1961ab76b1cae13d2b4f1bdc9f5e09e
Will Gundy <willgunderson503@gmail.com>
2022-05-27 09:29:34 -0700

    fix the react router in app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Link
 } from 'react-router-dom';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
