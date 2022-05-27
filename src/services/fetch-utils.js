f2d7ea54af38189d922c1685602989aea4b9c721
Beau Elliott <belliott1516@gmail.com>
2022-05-27 09:52:10 -0700

    added getPinballMachines in fetch utils

--- src/services/fetch-utils.js
+++ src/services/fetch-utils.js
@@ -1,6 +1,13 @@
 export async function getAllHomes(query) {
   const response = await fetch(`https://us-real-estate.p.rapidapi.com/v2/for-sale?limit=20&state_code=OR&city=${query}`);
-  const data = response.json();
+  const data = await response.json();
+
+  return data;
+}
+
+export async function getPinballMachines(query){ //eslint-disable-line
+  const response = await fetch();
+  const data = await response.json();
 
   return data;
 }
