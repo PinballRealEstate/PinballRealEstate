412758493a59df8436b9ac7d10ee52119edfe877
Beau Elliott <belliott1516@gmail.com>
2022-05-27 13:17:27 -0700

    added features to sign up page and added custom slider to components

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { signUp } from '../services/supabase-utils';
+import CustomSlider from './CustomSlider';
 
 export default function SignUp({ setUser }) {
 
   const [signUpData, setSignUpData] = useState({
+    username: '',
     email: '',
-    password: ''
+    password: '',
+    low_price: '',
+    high_price: '',
+    zip_code: '',
   });
 
   async function handleSignUp(e) {
@@ -21,6 +26,13 @@ export default function SignUp({ setUser }) {
     <div>
       <h1>Sign Up</h1>
       <form onSubmit={handleSignUp}>
+        <label> Username:
+          <input required type='username' onChange={(e) => setSignUpData({
+            username: e.target.value,
+            email: signUpData.email,
+            password: signUpData.password
+          })}/>
+        </label>
         <label>Email<input required type='email' onChange={ e => setSignUpData({
           email: e.target.value,
           password: signUpData.password
@@ -29,9 +41,21 @@ export default function SignUp({ setUser }) {
           email: signUpData.email,
           password: e.target.value
         })}/></label>
+        <label> Price Range: 
+          <CustomSlider />
+        </label>
+        <label> Zip Code: 
+          <input required type='number' onChange={(e) => setSignUpData({
+            username: e.target.value,
+            email: signUpData.email,
+            password: signUpData.password,
+            zip_code: e.target.value
+          })}/>
+        </label>
         <button>Sign Up</button>
       </form>
       <Link to={'/signin'}>Sign In</Link>
+      <Link to={'/about'}>About</Link>
     </div>
   );
 }
