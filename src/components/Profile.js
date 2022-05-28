17810304a7fa5171c730118c925914f7f62150a0
Khayman King <khaymanak@gmail.com>
2022-05-27 17:18:59 -0700

    more of the skeleton for profiles.js, need to add supabase utils function for editing username

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,23 +1,40 @@
 import React, { useEffect, useState } from 'react';
-import { getUser, getProfileByID } from '../services/supabase-utils';
+import { getUser, getProfileByID, getFilters, editUserName } from '../services/supabase-utils';
 
 export default function Profile() {
 
   const [profile, setProfile] = useState({});
-
+  const [visible, setVisible] = useState(false);
+  const [userNameData, setUserNameData] = useState('');
   useEffect(() => {
     async function getProfileOnLoad(){
       const { id } = await getUser();
-      console.log(id);
       const profileData = await getProfileByID(id);
+      await getFilters();
+      setProfile(profileData);
     }
+    return getProfileOnLoad;
   }, []);
-
-
+  
+  console.log('profile', profile);
+  async function handleNameChange(e) {
+    e.preventDefault();
+    await editUserName(userNameData);
+  }
 
   return (
     <div>
-      <p>profile</p>
+      <img src='https://placedog.net/200'/>
+      <h2>{profile.username}</h2>
+      <button onClick={(() => setVisible(true))}>Change User Name?</button>
+      <form>
+        { visible && <label>
+          Edit User Name
+          <input onChange={e => setUserNameData(e.target.value)} />
+          <button onClick={handleNameChange}>Change Name</button>
+        </label>
+        }
+      </form>
     </div>
   );
 }
