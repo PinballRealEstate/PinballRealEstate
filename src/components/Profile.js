e622d47410e92ca4b4d78d437ba196e9d2a93ebd
Khayman King <khaymanak@gmail.com>
2022-05-27 17:40:23 -0700

    more of the skeleton built out

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,25 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { getUser, getProfileByID, getFilters, editUserName } from '../services/supabase-utils';
+import { getUser, getProfileByID, getFilters, } from '../services/supabase-utils';
 
 export default function Profile() {
 
   const [profile, setProfile] = useState({});
   const [visible, setVisible] = useState(false);
-  const [userNameData, setUserNameData] = useState('');
+  const [filters, setFilters] = useState();
   useEffect(() => {
     async function getProfileOnLoad(){
       const { id } = await getUser();
       const profileData = await getProfileByID(id);
-      await getFilters();
+      const filterData = await getFilters();
       setProfile(profileData);
+      setFilters(filterData);
+      
     }
+    console.log('filters', filters);
     return getProfileOnLoad;
   }, []);
   
   console.log('profile', profile);
   async function handleNameChange(e) {
     e.preventDefault();
-    await editUserName(userNameData);
+    // await editUserName(userNameData);
   }
 
   return (
@@ -30,10 +33,12 @@ export default function Profile() {
       <form>
         { visible && <label>
           Edit User Name
-          <input onChange={e => setUserNameData(e.target.value)} />
+          <input onChange={e => setProfile.username(e.target.value)} />
           <button onClick={handleNameChange}>Change Name</button>
         </label>
         }
+        <div className='filters'>Filters go here</div>
+        <div className='cards'>Saved home cards here</div>
       </form>
     </div>
   );
