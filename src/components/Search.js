0247decf599f75ea7c9ff9e06d534a122cdd6019
Beau Elliott <belliott1516@gmail.com>
2022-05-31 14:10:09 -0700

    changed photo and adjusted map

--- src/components/Search.js
+++ src/components/Search.js
@@ -49,11 +49,11 @@ export default function Search() {
           <button onClick={handleSubmit}>Search</button>
         </form>
       </div>
-      <Mapbox/>
+      
       <div className='card-container'>
         {homes.map((home, i) => <PropertyCard key={i} home={home}> </PropertyCard>)}
       </div>
-      
+      <Mapbox/>
       <div>
        
       </div>
