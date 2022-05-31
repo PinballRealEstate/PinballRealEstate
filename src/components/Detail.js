3c350910edc78d835a0a4a0bc250a4e79d23ab74
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-05-31 14:38:57 -0700

    finally got tiny images slider dynamically adding the object images

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,9 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { results } from '../data';
+import TinySlider from 'tiny-slider-react';
+import 'tiny-slider/dist/tiny-slider.css';
+import SimpleImageSlider from 'react-simple-image-slider';
+
 
 export default function Detail() {
 
   const [details, setDetails] = useState({});
+
+  const [images, setImages] = useState([]);
+
+  const settings = {
+    lazyload: true,
+    nav: false,
+    mouseDrag: true,
+    controls: false
+  };
+
+  
+  
   
 
   useEffect(() => {
@@ -13,13 +29,25 @@ export default function Detail() {
       const aSingleHouse = results[0];
     }
     load();
-    console.log(results[0]);
-    console.log(details);
+    
+    const imageArray = results[0].photos.map((photo) =>{ return { 'url': photo.href };});
+    setImages(imageArray);
   }, []); 
+  console.log(images);  
+
 
   return (
     <div>
-      {results[0].photos.map((photo) => <img key={photo.href} src={photo.href}/>) }
+      { images.length > 1 && <SimpleImageSlider
+        width={896}
+        height={504}
+        images={images}
+        showBullets={true}
+        showNavs={true}
+      />}
+      {/* <TinySlider settings={settings}>
+        {results[0].photos.map((photo) => <img key={photo.href} src={photo.href}/>) }
+      </TinySlider> */}
       {}
       {/* <img src={results[0].photos[1].href}/> */}
       <p>{results[0].location.address.line}</p>
