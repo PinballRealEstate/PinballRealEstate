b2450d31b22557e57459672921825123d5241e3f
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-05-31 17:35:59 -0700

    added more details to detail page, added the definitely REAL distance of nearest pinball establishment

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -33,15 +33,12 @@ export default function Detail() {
     const imageArray = singleProperty.property_detail.photos.map((photo) => { return { 'url': photo.href };});
     setImages(imageArray);
   }, []); 
-  console.log(images);  
 
 
   return (
     <div className="detail-page">
       <h1>{results[0].location.address.line}</h1>
-      <h2 className="sub-heading">{singleProperty.property_detail.prop_common.sqft} SF | ${singleProperty.property_detail.prop_common.price} | {singleProperty.property_detail.address.city}, {singleProperty.property_detail.address.state}</h2>
-      {/* <h2 className="sub-heading">{results[0].branding[0].type} | ${results[0].list_price} | {results[0].location.address.city}, {results[0].location.address.state}</h2> */}
-
+      <h2 className="sub-heading">{singleProperty.property_detail.prop_common.sqft} SF | ${singleProperty.property_detail.prop_common.price.toLocaleString('en-US')}  (${Math.floor(singleProperty.property_detail.prop_common.price / singleProperty.property_detail.prop_common.sqft)}/SF) | {singleProperty.property_detail.address.city}, {singleProperty.property_detail.address.state}</h2>
       { images.length > 1 && <SimpleImageSlider
         width={896}
         height={504}
@@ -49,8 +46,25 @@ export default function Detail() {
         showBullets={true}
         showNavs={true}
       />}
-      {results[0].flags.is_new_listing && <p>This is a New Listing!</p>}
       <p>{singleProperty.property_detail.prop_common.description}</p>
+      <div className="property-deets">
+        <div>
+          <p>Bedrooms: <b>{singleProperty.property_detail.prop_common.bed}</b></p>
+          <p>Bathrooms: <b>{singleProperty.property_detail.prop_common.bath}</b></p>
+          <p>Buiding Height: <b>{singleProperty.property_detail.prop_common.stories} Story</b></p>
+          <p>Price Per SF: <b>${Math.floor(singleProperty.property_detail.prop_common.price / singleProperty.property_detail.prop_common.sqft)}</b></p>
+        </div>
+        <div>
+          <p>Property Type: <b>{singleProperty.property_detail.prop_common.type.charAt(0).toUpperCase() + singleProperty.property_detail.prop_common.type.slice(1).toLowerCase()}</b></p>
+          <p>Year Built: <b>{singleProperty.property_detail.prop_common.year_built}</b></p>
+          <p>Garage: <b>{singleProperty.property_detail.flags.is_garage_present ? 'Yes' : 'No' }</b></p>
+          <p>Nearest Pinball: <b>2.38 miles</b></p>
+        </div>
+      </div>
+      
+      
+
+      
     </div>
   );
 }
