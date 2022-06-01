5e2f4aa474bdbe5505ce5093e4752e86cdf63be1
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-06-01 15:56:48 -0700

    CSS styling and added school listings to detail page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -3,6 +3,7 @@ import SimpleImageSlider from 'react-simple-image-slider';
 import './Detail.css';
 import { useParams } from 'react-router-dom';
 import { getSingleHome } from '../services/fetch-utils';
+import { singleProperty } from '../single-property-data';
 
 
 export default function Detail() {
@@ -13,7 +14,7 @@ export default function Detail() {
 
   useEffect(() => {
     async function load() {
-      const { property_detail } = await getSingleHome(id);
+      const { property_detail } = singleProperty;//await getSingleHome(id);
       setDetails(property_detail);
       const imageArray = property_detail.photos.map((photo) => { return { 'url': photo.href };});
       setImages(imageArray);
@@ -27,8 +28,10 @@ export default function Detail() {
     <div className="detail-page">
       { details &&
       <div>
-        <h1>{details.address.line}</h1>
-        <h2 className="sub-heading">{details.prop_common.sqft} SF | ${details.prop_common.price.toLocaleString('en-US')}  (${Math.floor(details.prop_common.price / details.prop_common.sqft)}/SF) | {details.address.city}, {details.address.state}</h2>
+        <div className="address-area">
+          <h1 className="street-address">{details.address.line}</h1>
+          <h2 className="sub-heading">{details.prop_common.sqft} SF | ${details.prop_common.price.toLocaleString('en-US')}  (${Math.floor(details.prop_common.price / details.prop_common.sqft)}/SF) | {details.address.city}, {details.address.state}</h2>
+        </div>
         { images.length > 1 && <SimpleImageSlider
           width={896}
           height={504}
@@ -48,7 +51,13 @@ export default function Detail() {
             <p>Property Type: <b>{details.prop_common.type.charAt(0).toUpperCase() + details.prop_common.type.slice(1).toLowerCase()}</b></p>
             <p>Year Built: <b>{details.prop_common.year_built}</b></p>
             <p>Garage: <b>{details.flags.is_garage_present ? 'Yes' : 'No' }</b></p>
+            <p>Year Built: <b>{details.prop_common.year_built}</b></p>
             <p>Nearest Pinball: <b>2.38 miles</b></p>
+          </div>
+          <div>
+            <p>Nearby Schools:</p>
+            {details.schools.map(school => <p key={school.name}><b>{school.name}</b></p>)}
+            
           </div>
         </div> 
       </div>}
