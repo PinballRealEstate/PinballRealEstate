488c6b23c94f872f2956fedeb09ac8a7236f7599
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-06-02 16:47:51 -0700

    finished styling details elements

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -4,7 +4,6 @@ import './Detail.css';
 import { useParams } from 'react-router-dom';
 import { getSingleHome } from '../services/fetch-utils';
 import Mapbox from './Mapbox';
-import { singleProperty } from '../single-property-data.js';
 
 import Spinner from './Spinner';
 
@@ -18,7 +17,7 @@ export default function Detail() {
   useEffect(() => {
     async function load() {
       setIsLoading(true);
-      const { property_detail } = singleProperty;//await getSingleHome(id);
+      const { property_detail } = await getSingleHome(id);
       setDetails(property_detail);
       const imageArray = property_detail.photos.map((photo) => { return { 'url': photo.href };});
       setImages(imageArray);
@@ -47,24 +46,29 @@ export default function Detail() {
             />}
             <p className="deets">{details.prop_common.description}</p>
             <div className="property-deets">
-              <div>
+              <div className="deets-column">
+                <h2>Details:</h2>
                 <p>Bedrooms: <b>{details.prop_common.bed}</b></p>
                 <p>Bathrooms: <b>{details.prop_common.bath}</b></p>
                 <p>Buiding Height: <b>{details.prop_common.stories} Story</b></p>
                 <p>Price Per SF: <b>${Math.floor(details.prop_common.price / details.prop_common.sqft)}</b></p>
-                <p>Property Type: <b>{details.prop_common.type.charAt(0).toUpperCase() + details.prop_common.type.slice(1).toLowerCase()}</b></p>
+                <p>Property Type: <b>{details.prop_common.type.charAt(0).toUpperCase() + details.prop_common.type.slice(1).toLowerCase().replaceAll('_', ' ')}</b></p>
                 <p>Year Built: <b>{details.prop_common.year_built}</b></p>
                 <p>Garage: <b>{details.flags.is_garage_present ? 'Yes' : 'No' }</b></p>
                 <p>Year Built: <b>{details.prop_common.year_built}</b></p>
-                <p>Nearest Pinball: <b>2.38 miles</b></p>
               </div>
-              <div>
-                <p>Nearby Schools:</p>
-                {details.schools.map(school => <div key={school.name}><div className="school-item"><p><b>{school.name}</b></p><p>Grades:<b>{school.grades.range.low}-{school.grades.range.high}</b>   Distance:<b>{school.distance_in_miles}</b></p></div></div>)}
+              <div className="deets-column">
+                <h2>GreatSchools Ratings:</h2>
+                {details.schools.map(school => <div className="school-item-wrapper" key={school.name}>
+                  {school.ratings.great_schools_rating ?
+                    <p className="school-grade"><b className="school-value">{school.ratings.great_schools_rating}</b>/10</p>
+                    : <p className="school-grade"><b className="school-value">N/A</b></p>}
+                  <div className="school-item" ><p><b>{school.name}</b></p><p>Grades:<b>{school.grades.range.low}-{school.grades.range.high}</b>   Distance:<b>{school.distance_in_miles}</b></p></div></div>)}
               </div>
-              <div className="table-holder">
+              <div className="deets-column" >
+                <h2>Listing History:</h2>
                 <div className="row-class2"><p><b>Date</b></p><p><b>Price</b></p><p><b>Event</b></p></div>
-                { details.property_history.map(history => <div className="row-class" key={history.source}><p>{history.date}</p><p>$ {history.price.toLocaleString('en-US')}</p><p>{history.event_name}</p></div>)}
+                { details.property_history.map(history => <div className="row-class" key={history.source}><p><b>{history.date}</b></p><p>$ {history.price.toLocaleString('en-US')}</p><p>{history.event_name}</p></div>)}
                 
                 
               </div>
