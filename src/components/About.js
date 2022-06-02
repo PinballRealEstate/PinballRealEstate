0f9c0e4a340dd32df34741359b2062f408268ff4
Will Gundy <willgunderson503@gmail.com>
2022-06-02 16:30:53 -0700

    about us

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import './About.css';
+import AboutCard from './AboutCard';
 
 export default function About() {
   return (
-    <div>About</div>
+    <main className='about-main'>
+      <div className='about-container'>
+        <AboutCard 
+          name={'Beau Elliott'} 
+          image_url={'../assets/Beau-headshot copy.png'} 
+          bio={'Beau currently resides in Seattle, WA and loves eating ramen, teaching pole dance, and rock climbing.'} 
+          architecture={''} 
+          pinball={''} 
+          github_link={'https://github.com/belliott15'} 
+          linkedin_link={'https://www.linkedin.com/in/beau-elliott15/'}/>
+        <AboutCard 
+          name={'Khayman King'} 
+          image_url={'../assets/khaymanHeadshot.png'} 
+          bio={''} 
+          architecture={''} 
+          pinball={''} 
+          github_link={''} 
+          linkedin_link={''}/>
+        <AboutCard 
+          name={'Elliott Cheifetz'} 
+          image_url={'../assets/elliottHeadshot.png'} 
+          bio={''} 
+          architecture={''} 
+          pinball={''}  
+          github_link={''} 
+          linkedin_link={''}/>
+        <AboutCard 
+          name={'Will Gunderson'} 
+          image_url={'../assets/willHeadshot.png'} 
+          bio={'Will is a software developer in training, currently living in Bend, OR.'} 
+          architecture={'Cross Gabled Craftsman'} 
+          pinball={'The Addams Family'} 
+          github_link={'https://github.com/willgundy'} 
+          linkedin_link={'https://www.linkedin.com/in/will-gunderson/'}/>
+      </div>
+    </main>
   );
 }
