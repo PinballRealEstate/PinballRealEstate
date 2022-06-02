596821f13243e23dd74414aea1f960f36fdf01cf
Will Gundy <willgunderson503@gmail.com>
2022-06-01 17:46:49 -0700

    changed styling on detail page

--- src/components/Mapbox.js
+++ src/components/Mapbox.js
@@ -55,7 +55,7 @@ export default function Mapbox({ homes, initial_lat, initial_lon }) {
           latitude: initial_lat,
           zoom: 10
         }}
-        style={{ width: '100vw', height: '300px' }}
+        style={{ width: '70vw', height: '300px' }}
         mapStyle="mapbox://styles/willgundy/cl3951tjg000014o8h75x3u7n"
         mapboxAccessToken={'pk.eyJ1Ijoid2lsbGd1bmR5IiwiYSI6ImNsMzNtd3RwZDAyaDAzYm0xa2F5bWRxd2UifQ.K6k7FavnWDdnUB_CVEIzzA'}
       >
